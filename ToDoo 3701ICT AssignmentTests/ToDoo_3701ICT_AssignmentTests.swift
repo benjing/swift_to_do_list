3b5d944d23e0f606596679d52d43e059a6555d55
lynmaree <emma-jeaninglis@lyn-marees-macbook.local>
2018-04-05 13:35:57 +1000

    Initial Commit

---
+++ ToDoo 3701ICT AssignmentTests/ToDoo_3701ICT_AssignmentTests.swift
@@ -0,0 +1,36 @@
+//
+//  ToDoo_3701ICT_AssignmentTests.swift
+//  ToDoo 3701ICT AssignmentTests
+//
+//  Created by lynmaree on 5/4/18.
+//  Copyright © 2018 Benjamin. All rights reserved.
+//
+
+import XCTest
+@testable import ToDoo_3701ICT_Assignment
+
+class ToDoo_3701ICT_AssignmentTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
